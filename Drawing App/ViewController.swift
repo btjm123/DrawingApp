73124e1c9b0369ab0c1f06e5f4ef624a7e3c3554
roycetanjiashing <roycetan@roycetanjiashings-MacBook-Pro.local>
2016-12-08 16:24:58 +0800

    Initial Commit

---
+++ Drawing App/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Drawing App
+//
+//  Created by roycetanjiashing on 17/9/16.
+//  Copyright © 2016 examplecompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var lastPoint = CGPoint.zero
+    var swiped = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    func drawLines(fromPoint:CGPoint,toPoint:CGPoint) {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.move(to: CGPoint(x: fromPoint.x, y: fromPoint.y))
+        context?.addLine(to: CGPoint(x: toPoint.x, y: toPoint.y))
+        
+        context?.setBlendMode(CGBlendMode.normal)
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(5)
+        context?.setStrokeColor(UIColor(red: 0, green: 0, blue: 0, alpha: 1.0).cgColor)
+        
+        context?.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: self.view)
+            drawLines(fromPoint: lastPoint, toPoint: currentPoint)
+            
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            drawLines(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# DrawingApp
+Simple Drawing App written in Swift 3 and Xcode 8. 
+
+This repository complements a series called 'Make a Drawing App' on Youtube,where you find Swift 3 tutorials over there.
+Feel free to check it out!
+My Youtube channel (BR Studio - Coding) link : https://www.youtube.com/channel/UClmRwdRb2PC6D3gJM-fpYUw
+
+#Progress
+<img width="370" alt="screen shot 2016-09-19 at 11 52 28 am" src="https://cloud.githubusercontent.com/assets/19306879/18622273/93f777fa-7e5f-11e6-86f5-742c763b92c9.png">
+
+- Primitive and Simple Drawing
+
+#TO BE IMPLEMENTED
+- Share your image via Facebook using Social Framework or UIActivityViewController
+- Erase parts of your drawing
+- Reset your drawing
+- Save your artwork into your photos(and redirect users to Settings page if not enabled app to access photos)
+- Provide some colour presets.
+- Create a settings view controller to add more customization(such as change brush size,colours etc.)
+
